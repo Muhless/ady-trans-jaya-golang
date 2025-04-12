207a9351250c932c624b87d3766927c3a12a1d4d
Muhless <nuryadi.muhta@gmail.com>
2025-04-12 10:59:32 +0700

    make user table

--- internal/db/connect.go
+++ internal/db/connect.go
@@ -3,7 +3,9 @@ package db
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -11,11 +13,29 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "host=localhost user=postgres password=MUHLESS717GG dbname=ady-trans-jaya port=5432 sslmode=disable Timezone=Asia/Jakarta"
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(".env file not found")
+	}
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable Timezone=Asia/Jakarta",
+		getEnv("DB_HOST", "localhost"),
+		getEnv("DB_USER", "postgres"), 
+		getEnv("DB_PASSWORD", "MUHLESS717GG"), 
+		getEnv("DB_NAME", "ady-trans-jaya"), 
+		getEnv("DB_PORT", "5432"),
+	)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed connect to Database: %v", err)
 	}
 	fmt.Println("Succesfully connect to Database")
 	DB = db
+}
+
+func getEnv(key, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return fallback
 }
