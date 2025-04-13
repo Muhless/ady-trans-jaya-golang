cf2ec5f8311f43e35d73b80a35ed147f27b0ebc5
Muhless <nuryadi.muhta@gmail.com>
2025-04-13 11:08:58 +0700

    connect to frontend

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"ady-trans-jaya-golang/internal/db"
+	"net/http"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	db.Connect()
+	// db.Connect()
+	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+	r.GET("/api/hello", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Hello from golang API",
+		})
+	})
+
+	r.Run(":8080")
 }
